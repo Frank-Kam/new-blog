0bc2e6f98c7b85b5e46cbb462f27d99c897e0116
Frank-Kam <ganl68384@gmail.com>
2025-08-30 00:58:45 +0800

    feat(rss): sort posts by published date in descending order

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,9 +1,11 @@
+// src/pages/rss.xml.ts
 import rss from '@astrojs/rss'
 import siteConfig from '~/site.config'
 import type { AstroGlobal } from 'astro'
-import { getSortedPosts } from '~/utils'
+import { getCollection } from 'astro:content'
 import sanitizeHtml from 'sanitize-html'
 import MarkdownIt from 'markdown-it'
+
 const parser = new MarkdownIt()
 
 // https://docs.astro.build/en/recipes/rss/
@@ -14,7 +16,14 @@ export async function GET(_context: AstroGlobal) {
     )
     return
   }
-  const posts = await getSortedPosts()
+
+  
+  const posts = (await getCollection('posts')).sort(
+    (a, b) =>
+      new Date(b.data.published).getTime() -
+      new Date(a.data.published).getTime(),
+  )
+
   return rss({
     stylesheet: '/rss.xsl',
     title: siteConfig.title,
@@ -25,7 +34,7 @@ export async function GET(_context: AstroGlobal) {
       pubDate: post.data.published,
       description: post.data.description,
       author: post.data.author || siteConfig.author,
-      link: `/posts/${post.id}`,
+      link: `/posts/${post.slug}`,
       content: sanitizeHtml(parser.render(post.body || ''), {
         allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
       }),
