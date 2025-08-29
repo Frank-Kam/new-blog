3913208a5ce2de4d5fda8f906c6355d565a5d413
Frank-Kam <ganl68384@gmail.com>
2025-08-30 01:45:50 +0800

    feat(social-card): add Chinese font support for OG image

--- src/pages/social-cards/[slug].png.ts
+++ src/pages/social-cards/[slug].png.ts
@@ -10,7 +10,7 @@ import type { ReactNode } from 'react'
 
 // Load the font file as binary data
 const fontPath = path.resolve(
-  './node_modules/@expo-google-fonts/jetbrains-mono/400Regular/JetBrainsMono_400Regular.ttf',
+  './node_modules/@expo-google-fonts/noto-sans-tc/900Black/NotoSansTC_900Black.ttf',
 )
 const fontData = fs.readFileSync(fontPath) // Reads the file as a Buffer
 
