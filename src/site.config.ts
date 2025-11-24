a30b5c92add5d295c1e96606e19abccb8eb3088b
Frank-Kam <ganl68384@gmail.com>
2025-11-24 22:53:02 +0800

    chore(config): update site title

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,7 +4,7 @@ const config: SiteConfig = {
   // Absolute URL to the root of your published site, used for generating links and sitemaps.
   site: 'https://blog.frk.tw',
   // The name of your site, used in the title and for SEO.
-  title: 'Frank\'s Blog',
+  title: 'Frank Blog',
   // The description of your site, used for SEO and RSS feed.
   description:
     'A CTF Player & Security Researcher & Backend Developer',
